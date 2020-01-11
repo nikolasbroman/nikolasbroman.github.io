76598b1994364bac0a9ae7aaccc53634f089fdcf
Nikolas Broman <nikolas.broman@gmail.com>
2020-01-11 21:24:42 +0200

    Add publishable HTML and CSS

---
+++ bg-image-size.js
@@ -0,0 +1,15 @@
+let width = window.innerWidth;
+let height = window.innerHeight;
+let body = document.body;
+
+if (width <= 640 && height <= 352) {
+  body.style.backgroundImage = "url(images/bg-640-352.jpg)";
+} else if (width <= 800 && height <= 440) {
+  body.style.backgroundImage = "url(images/bg-800-440.jpg)";
+} else if (width <= 1280 && height <= 704) {
+  body.style.backgroundImage = "url(images/bg-1280-704.jpg)";
+} else if (width <= 1440 && height <= 792) {
+  body.style.backgroundImage = "url(images/bg-1440-792.jpg)";
+} else if (width <= 1600 && height <= 880) {
+  body.style.backgroundImage = "url(images/bg-1600-880.jpg)";
+}
